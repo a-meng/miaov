426d2fa1ee58ca72c5312aff1f59cf7217cb5b92
ameng <zm271158316>
2016-05-24 16:20:03 +0800

    移动页面练习

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ var path=require('path');
 
 module.exports = {
     entry: {
-        huandengpian:"./huandengpian/main.es6",
-        web1:"./web1/main.es6"
+        huandengpian:"./huandengpian/main.es6", //幻灯片练习
+        web1:"./web1/main.es6", //移动页面练习1
+        shop:'./shop/main.es6' //移动页面购物网站练习
     },
     output: {
         path: path.join(__dirname, "js"),
