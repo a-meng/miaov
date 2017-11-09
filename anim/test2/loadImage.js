4eeb82c6321f2cdb82d8591e6807968770fad3ed
zhengmeng <zm271158316>
2017-11-09 17:24:39 +0800

    anim练习

--- anim/test2/loadImage.js
+++ anim/test2/loadImage.js
@@ -2,10 +2,8 @@ function loadImage(srcList) {
     return srcList.map(function (src) {
         return new Promise(function (resolve, reject) {
             let img = new Image();
+            img.onload = resolve;
 
-            img.onload = function () {
-                resolve(src);
-            };
             img.onerror = reject;
             img.src = src;
         });
